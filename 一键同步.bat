f63940eec614ea6459b29d33dad69bea428d1fbf
scallopshark <15470090+scallopshark@user.noreply.gitee.com>
2026-04-07 16:13:28 +0800

    提交更新

--- 一键同步.bat
+++ 一键同步.bat
@@ -1,5 +1,7 @@
+@echo off
+chcp 65001 >nul
 git add .
 git commit -m "提交更新"
 git remote set-url origin git@github.com:ScallopShark-ai/papercatch.git
-git push origin master
+git push origin main
 pause
